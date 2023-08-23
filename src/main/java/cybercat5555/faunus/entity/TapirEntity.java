454c103027828ce7d30da3ac4d42eb3186e957c1
Lafolie <lafoliex7@aol.com>
2023-08-23 07:47:39 +0100

    rudimentary AI. Kinda works too...

--- src/main/java/cybercat5555/faunus/entity/TapirEntity.java
+++ src/main/java/cybercat5555/faunus/entity/TapirEntity.java
@@ -1,6 +1,11 @@
 package cybercat5555.faunus.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.brain.Brain;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -8,6 +13,24 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
+
+import net.tslat.smartbrainlib.api.SmartBrainOwner;
+import net.tslat.smartbrainlib.api.core.BrainActivityGroup;
+import net.tslat.smartbrainlib.api.core.SmartBrainProvider;
+import net.tslat.smartbrainlib.api.core.behaviour.FirstApplicableBehaviour;
+import net.tslat.smartbrainlib.api.core.behaviour.OneRandomBehaviour;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.look.LookAtTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.misc.Idle;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.move.FleeTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.move.MoveToWalkTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.path.SetRandomWalkTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.target.InvalidateAttackTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.target.SetPlayerLookTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.target.SetRandomLookTarget;
+import net.tslat.smartbrainlib.api.core.sensor.ExtendedSensor;
+import net.tslat.smartbrainlib.api.core.sensor.vanilla.HurtBySensor;
+import net.tslat.smartbrainlib.api.core.sensor.vanilla.NearbyLivingEntitySensor;
+
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.core.animation.AnimationController;
@@ -17,7 +40,7 @@ import software.bernie.geckolib.core.animation.AnimatableManager.ControllerRegis
 import software.bernie.geckolib.core.object.PlayState;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
-public class TapirEntity extends AnimalEntity implements GeoEntity
+public class TapirEntity extends AnimalEntity implements GeoEntity, SmartBrainOwner<TapirEntity>
 {
 	protected static final RawAnimation IDLE_ANIM = RawAnimation.begin().thenLoop("idle");
 
@@ -33,6 +56,67 @@ public class TapirEntity extends AnimalEntity implements GeoEntity
 	{
 		// TODO Auto-generated method stub
 		throw new UnsupportedOperationException("Unimplemented method 'createChild'");
+	}
+
+	@Override
+	protected Brain.Profile<?> createBrainProfile()
+	{
+		return new SmartBrainProvider<>(this);
+	}
+
+	@Override
+	public List<? extends ExtendedSensor<? extends TapirEntity>> getSensors()
+	{
+		return ObjectArrayList.of
+		(
+			new NearbyLivingEntitySensor<>(),
+			new HurtBySensor<>()
+		);
+	}
+
+	@Override
+	public BrainActivityGroup<TapirEntity> getCoreTasks()
+	{
+		return BrainActivityGroup.coreTasks
+		(
+			new LookAtTarget<>(),
+			new MoveToWalkTarget<>()
+		);
+	}
+
+	@Override
+	public BrainActivityGroup<TapirEntity> getIdleTasks()
+	{
+		return BrainActivityGroup.idleTasks
+		(
+			new FirstApplicableBehaviour<TapirEntity>
+			(
+				new SetPlayerLookTarget<>(),
+				new SetRandomLookTarget<>()
+			),
+			new OneRandomBehaviour<TapirEntity>
+			(
+				new SetRandomWalkTarget<>(),
+				new Idle<>().runFor(entity -> entity.getRandom().nextBetween(30, 60))
+			)
+		);
+	}
+
+	@Override
+	public BrainActivityGroup<TapirEntity> getFightTasks()
+	{
+		return BrainActivityGroup.fightTasks
+		(
+			new InvalidateAttackTarget<>(),
+			new FleeTarget<>()
+		);
+	}
+
+	@Override
+	public void mobTick()
+	{
+		super.mobTick();
+		tickBrain(this);
 	}
 
 	@Override
