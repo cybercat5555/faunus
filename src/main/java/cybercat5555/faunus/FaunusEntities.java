85dd948f84a8ffb8610799f1270efe1ff88c29e8
Lafolie <lafoliex7@aol.com>
2023-08-30 16:08:52 +0100

    simplified entity registration

--- src/main/java/cybercat5555/faunus/FaunusEntities.java
+++ src/main/java/cybercat5555/faunus/FaunusEntities.java
@@ -6,6 +6,7 @@ import cybercat5555.faunus.entity.TapirEntity;
 import cybercat5555.faunus.util.FaunusID;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
@@ -16,37 +17,27 @@ public final class FaunusEntities
 {
 	private FaunusEntities() {}
 
-	public static final EntityType<TapirEntity> TAPIR = Registry.register
-	(
-		Registries.ENTITY_TYPE, 
-		FaunusID.content("tapir"), 
-		FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, TapirEntity::new)
+	public static final EntityType<TapirEntity> TAPIR = register("tapir", FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, TapirEntity::new)
 			.dimensions(EntityDimensions.fixed(0.9f, 1.4f))
-			.build()
-	);
+			.build());
 
-	public static final EntityType<PiranhaEntity> PIRANHA = Registry.register
-	(
-		Registries.ENTITY_TYPE,
-		FaunusID.content("piranha"),
-		FabricEntityTypeBuilder.create(SpawnGroup.WATER_CREATURE, PiranhaEntity::new)
-			.dimensions(EntityDimensions.fixed(0.5f, 0.5f))
-			.build()
-	);
+	public static final EntityType<PiranhaEntity> PIRANHA = register("piranha", FabricEntityTypeBuilder.create(SpawnGroup.WATER_CREATURE, PiranhaEntity::new)
+		.dimensions(EntityDimensions.fixed(0.5f, 0.5f))
+		.build());
 
-	public static final EntityType<ArapaimaEntity> ARAPAIMA = Registry.register
-	(
-		Registries.ENTITY_TYPE,
-		FaunusID.content("arapaima"),
-		FabricEntityTypeBuilder.create(SpawnGroup.WATER_CREATURE, ArapaimaEntity::new)
-			.dimensions(EntityDimensions.fixed(1.2f, 0.5f))
-			.build()
-	);
+	public static final EntityType<ArapaimaEntity> ARAPAIMA = register("arapaima", FabricEntityTypeBuilder.create(SpawnGroup.WATER_CREATURE, ArapaimaEntity::new)
+		.dimensions(EntityDimensions.fixed(1.2f, 0.5f))
+		.build());
 
 	public static void init()
 	{
 		FabricDefaultAttributeRegistry.register(TAPIR, TapirEntity.createMobAttributes());
 		FabricDefaultAttributeRegistry.register(PIRANHA, PiranhaEntity.createFishAttributes());
 		FabricDefaultAttributeRegistry.register(ARAPAIMA, ArapaimaEntity.createFishAttributes());
+	}
+
+	private static <T extends Entity> EntityType<T> register(String name, EntityType<T> type)
+	{
+		return Registry.register(Registries.ENTITY_TYPE, FaunusID.content(name), type);
 	}
 }
